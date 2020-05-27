b4ee701da206d4f654826ae97be1639f12a53b48
cjl_macbook <cjl_spy@163.com>
2020-05-27 20:36:00 +0800

    typo

--- example/bin/main.dart
+++ example/bin/main.dart
@@ -2,20 +2,20 @@ import 'dart:io';
 
 import 'package:image_size_getter/image_size_getter.dart';
 
-main(List<String> arguments) async {
-  File file = File("asset/IMG_20180908_080245.jpg");
+void main(List<String> arguments) async {
+  final file = File('asset/IMG_20180908_080245.jpg');
   final size = ImageSizGetter.getSize(file);
-  print("jpg = $size");
+  print('jpg = $size');
 
-  File pngFile = File("asset/ic_launcher.png");
+  final pngFile = File('asset/ic_launcher.png');
   final pngSize = ImageSizGetter.getSize(pngFile);
-  print("png = $pngSize");
+  print('png = $pngSize');
 
-  File webpFile = File("asset/demo.webp");
+  final webpFile = File('asset/demo.webp');
   final webpSize = ImageSizGetter.getSize(webpFile);
-  print("webp = $webpSize");
+  print('webp = $webpSize');
 
-  File gifFile = File("asset/dialog.gif");
+  final gifFile = File('asset/dialog.gif');
   final gifSize = ImageSizGetter.getSize(gifFile);
-  print("gif = $gifSize");
+  print('gif = $gifSize');
 }
