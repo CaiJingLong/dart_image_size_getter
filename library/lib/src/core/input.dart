25d236462fc3502c754048210ae7c9dcc629579e
caijinglong-b-imac <cjl_spy@163.com>
2022-03-31 15:18:12 +0800

    SyncInputWrapper

--- library/lib/src/core/input.dart
+++ library/lib/src/core/input.dart
@@ -11,9 +11,30 @@ abstract class ImageInput {
 abstract class AsyncImageInput {
   const AsyncImageInput();
 
+  factory AsyncImageInput.input(ImageInput input) = _SyncInputWrapper;
+
   Future<int> get length;
 
   Future<List<int>> getRange(int start, int end);
 
   Future<bool> exists();
+}
+
+class _SyncInputWrapper extends AsyncImageInput {
+  final ImageInput _input;
+
+  const _SyncInputWrapper(this._input);
+
+  @override
+  Future<bool> exists() async {
+    return _input.exists();
+  }
+
+  @override
+  Future<List<int>> getRange(int start, int end) async {
+    return _input.getRange(start, end);
+  }
+
+  @override
+  Future<int> get length async => _input.length;
 }
