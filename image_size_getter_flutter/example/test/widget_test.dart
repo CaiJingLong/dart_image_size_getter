04facce6d5f5cb6f126e8222377bbe15d92c6fdd
cjl_macbook <cjl_spy@163.com>
2022-04-10 22:18:15 +0800

    Add test

--- image_size_getter_flutter/example/test/widget_test.dart
+++ image_size_getter_flutter/example/test/widget_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:image_size_getter/image_size_getter.dart';
 
